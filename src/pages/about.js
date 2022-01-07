904c02efaca5b8f10fb0e98badf3309f63b0291a
Tim Sandberg <tasandberg@gmail.com>
2022-01-07 10:25:58 -0800

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,43 @@
 import * as React from "react"
+import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Figure } from "react-bootstrap"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
 const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>About</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+  <Layout title="About">
+    <p>
+      <small>
+        <i>Warning: insufferable third-persion bio incoming</i>
+      </small>
+    </p>
+    <p>
+      Tim (he/him) is a senior software engineer, primarily focused on
+      full-stack web technologies like Ruby on Rails, Reactjs, React-Native, and
+      various DevOps tools like docker and CI pipelines. He's also been accused
+      of having an eye for design, and some skill with CSS3.
+    </p>
+    <p>
+      More about Tim's work history <Link to="/work">here</Link>.
+    </p>
+    <p>
+      When he's not coding, Tim is usually making music. These days he is
+      focused on composing music for film and other media. Check that out{" "}
+      <a href="https://timsandberg.com">here</a>. He goes wilderness backpacking
+      a few times a year and enjoy photography. Sometimes sometimes he develops
+      his own film:
+    </p>
+    <Figure>
+      <StaticImage
+        className="figure-img img-fluid rounded"
+        src="../images/tim-film.jpg"
+        alt="Man with strange hat standing in the woods"
+      />
+      <Figure.Caption>
+        This one taken by Tim's daughter, what a natural!
+      </Figure.Caption>
+    </Figure>
+    <p>He lives in Oakland, CA with his partner and daughter.</p>
   </Layout>
 )
 
